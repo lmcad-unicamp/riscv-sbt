9ca7d647eb21773410a1a5ab73bc13e356481bfe
luporl <leandro.lupori@gmail.com>
2017-02-07 18:56:51 -0200

    Reorganizing Makefiles, part I.

---
+++ config.mk
@@ -0,0 +1,72 @@
+### config ###
+
+# flags
+CFLAGS    := -Wall -Werror -O2
+CXXFLAGS  := $(CFLAGS)
+
+# build type dir
+ifeq ($(BUILD_TYPE),Debug)
+  BUILD_TYPE_DIR := debug
+else
+  BUILD_TYPE_DIR := release
+endif
+
+# SBT
+SBT_BUILD_DIR := $(TOPDIR)/build/sbt/$(BUILD_TYPE_DIR)
+X86_SYSCALL_O := $(SBT_BUILD_DIR)/CMakeFiles/x86_syscall.dir/x86_syscall.s.o
+
+#
+# tools
+#
+
+TOOLCHAIN         := $(TOPDIR)/toolchain
+TOOLCHAIN_RELEASE := $(TOOLCHAIN)/release
+TOOLCHAIN_X86     := $(TOOLCHAIN)/x86
+
+RV32_TRIPLE   := riscv32-unknown-elf
+RV64_TRIPLE   := riscv64-unknown-elf
+X86_TRIPLE    := i386-unknown-elf
+
+RV32_AS   := $(RV32_TRIPLE)-as
+RV32_LD   := $(RV32_TRIPLE)-ld
+RV32_GCC  := $(RV32_TRIPLE)-gcc
+
+RV64_AS   := $(RV64_TRIPLE)-as
+RV64_LD   := $(RV64_TRIPLE)-ld
+RV64_GCC  := $(RV64_TRIPLE)-gcc
+
+RV32_RUN  := LD_LIBRARY_PATH=$(TOOLCHAIN_RELEASE)/lib spike $(PK32)
+RV64_RUN  := LD_LIBRARY_PATH=$(TOOLCHAIN_RELEASE)/lib spike --isa=RV64IMAFDC pk
+
+X86_AS    := $(X86_TRIPLE)-as
+X86_LD    := $(X86_TRIPLE)-ld
+
+# clang
+CLANG := clang
+
+# RISC-V 32 bit
+RV32_CLANG        := $(CLANG)
+RV32_SYSROOT      := $(TOOLCHAIN_RELEASE)/$(RV32_TRIPLE)
+RV32_SYSROOT_FLAG := -isysroot $(RV32_SYSROOT) \
+                     -isystem $(RV32_SYSROOT)/include
+RV32_CLANG_FLAGS  := --target=riscv32 -mriscv=RV32IAMFD \
+                     $(RV32_SYSROOT_FLAG)
+RV32_LIB  := $(TOOLCHAIN_RELEASE)/$(RV32_TRIPLE)/lib
+RV32_CRT0 := $(RV32_LIB)/crt0.o
+RV32_LD_FLAGS0 := -L$(RV32_LIB) -dT ldscripts/elf32lriscv.x $(RV32_CRT0)
+RV32_LD_FLAGS1 := -lc -lgloss -lc
+
+# x86
+X86_CLANG       := $(CLANG)
+X86_CLANG_FLAGS := --target=i386 -fno-exceptions -fno-rtti
+X86_LIB         := /usr/lib32
+X86_LIB_GCC     := $(TOOLCHAIN_X86)/lib/gcc/$(X86_TRIPLE)/7.0.0
+X86_CRT1        := $(X86_LIB)/crt1.o
+X86_CRTI        := $(X86_LIB)/crti.o
+X86_CRTBEGIN    := $(X86_LIB_GCC)/crtbegin.o
+X86_CRTEND      := $(X86_LIB_GCC)/crtend.o
+X86_CRTN        := $(X86_LIB)/crtn.o
+X86_LGCC        := $(X86_LIB_GCC)/libgcc.a
+X86_LC          := $(X86_LIB)/libc.a
+X86_LD_FLAGS0   := $(X86_CRT1) $(X86_CRTI)
+X86_LD_FLAGS1   := $(X86_LC) $(X86_LGCC) $(X86_LC) $(X86_CRTN)
---
+++ rules.mk
@@ -0,0 +1,50 @@
+### rules ###
+
+# CLEAN
+# 1: module
+# 2: clean .s?
+define CLEAN
+clean-$(1):
+	rm -f $(1) $(1).o $$(if $(2),$(1).s,)
+endef
+
+# RUN
+# 1: prefix
+# 2: program
+define RUN
+run-$(2): $(2)
+ifeq ($$($(1)_RUN),)
+	./$(2)
+else
+	$$($(1)_RUN) $(2)
+endif
+endef
+
+# BUILD
+# 1: prefix
+# 2: output module
+# 3: [opt] GCC/CLANG
+# 4: [opt] source .c file
+define BUILD
+# .c -> .s
+ifneq ($(4),)
+$(2).s: $(4).c
+	$$($(1)_$(3)) $$($(1)_$(3)_FLAGS) $(CFLAGS) -o $$@ -S $$<
+endif
+
+# .s -> .o
+$(2).o: $(2).s
+	$$($(1)_AS) -o $$@ -c $$<
+
+# .o -> elf
+ifneq ($(4),)
+$(2): $(2).o
+	$$($(1)_LD) -o $$@ $$($(1)_LD_FLAGS0) $$< $$($(1)_LD_FLAGS1)
+else
+$(2): $(2).o
+	$$($(1)_LD) -o $$@ $$<
+endif
+
+$(call RUN,$(1),$(2))
+$(call CLEAN,$(2),$(4))
+endef
