0764079b953d71cbc24991600e21414f1b22d574
luporl <leandro.lupori@gmail.com>
2017-07-25 20:14:10 -0300

    measuring 10 runs and taking the median

--- scripts/measure.py
+++ scripts/measure.py
@@ -9,8 +9,9 @@ TARGETS = [ 'x86' ]
 class TestData:
     def __init__(self, bin):
         self.bin = bin
-        self.out = "/tmp/" + bin + ".out"
-        self.time = 0.0
+
+    def out(self, i):
+        return "/tmp/" + self.bin + str(i) + ".out"
 
 
 class Test:
@@ -24,32 +25,71 @@ class Test:
         self.native = TestData(bin)
         self.rv32 = TestData("rv32-" + bin)
 
-    def run1(self, td):
-        print("measuring", td.bin)
-        path = self.dir + '/' + td.bin
-        args = [path]
-        args.extend(self.args)
-        print(args)
-        sys.stdout.flush()
-        with open(td.out, 'w') as f:
+    def run1(self, args, td, i):
+        with open(td.out(i), 'w') as f:
             t0 = time.time()
             subprocess.check_call(args, stdout=f)
             t1 = time.time()
             t = t1 - t0
-        print("time taken:", t)
-        td.time = t
+        print("run #" + str(i) + ": time taken:", t)
+        sys.stdout.flush()
+        return t
 
 
     def run(self):
-        self.run1(self.native)
-        self.run1(self.rv32)
-        rc = subprocess.call(["diff", self.native.out, self.rv32.out])
-        if rc:
-            sys.exit("ERROR: translated output differs from native")
-        nt = self.native.time
-        rt = self.rv32.time
-        oh = (rt - nt) / nt
-        print("overhead=", oh, sep='')
+        def prep(td):
+            print("measuring", td.bin)
+
+            path = self.dir + '/' + td.bin
+            args = [path]
+            args.extend(self.args)
+            print(" ".join(args))
+            sys.stdout.flush()
+            return args
+
+        n = 10
+
+        # native
+        td = self.native
+        args = prep(td)
+
+        ntimes = []
+        for i in range(n):
+            t = self.run1(args, td, i)
+            ntimes.append(t)
+
+        # translated
+        td = self.rv32
+        args = prep(td)
+
+        ttimes = []
+        for i in range(n):
+            t = self.run1(args, td, i)
+            ttimes.append(t)
+
+        # compare outputs
+        for i in range(n):
+            rc = subprocess.call(["diff", self.native.out(i), self.rv32.out(i)])
+            if rc:
+                sys.exit("ERROR: translated output differs from native (run #"
+                         + str(i) + ")")
+
+        def median(vals):
+            vals.sort()
+            n = len(vals)
+            if n % 2 == 0:
+                v1 = vals[n//2 -1]
+                v2 = vals[n//2]
+                return (v1 + v2) / 2
+            else:
+                return vals[n//2 +1]
+
+        nt = median(ntimes)
+        tt = median(ttimes)
+        oh = (tt - nt) / nt
+        print("native time     =", nt)
+        print("translated time =", tt)
+        print("overhead        =", oh)
 
 
 def measure(target, dir, test_name, args):
