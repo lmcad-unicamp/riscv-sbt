2e3464df09fb002da11899fa0dfa504924d60fd2
luporl <leandro.lupori@gmail.com>
2017-07-25 10:00:28 -0300

    dijkstra running until the end, but output is wrong

--- sbt/libc.c
+++ sbt/libc.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #define F(x) \
-  auto rv32_##x = x;
+  void* rv32_##x = x;
 
 F(abort)
 F(exit)
@@ -17,4 +17,3 @@ F(perror)
 F(printf)
 F(putchar)
 F(puts)
-// F(__isoc99_fscanf)
